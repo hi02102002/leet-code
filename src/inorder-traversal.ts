ec57b185482cb645e69c65fdefabb7a03b5677b4
Võ Hoàng Huy <2051120240@ut.edu.vn>
2024-01-24 01:42:02 +0700

    94. Binary Tree Inorder Traversal

---
+++ src/inorder-traversal.ts
@@ -0,0 +1,33 @@
+class TreeNode {
+   val: number;
+   left: TreeNode | null;
+   right: TreeNode | null;
+   constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+      this.val = val === undefined ? 0 : val;
+      this.left = left === undefined ? null : left;
+      this.right = right === undefined ? null : right;
+   }
+}
+
+/**
+ * 94. Binary Tree Inorder Traversal
+ * @url https://leetcode.com/problems/binary-tree-inorder-traversal
+ * @param root
+ * @returns
+ */
+function inorderTraversal(root: TreeNode | null): number[] {
+   const result: number[] = [];
+
+   const helper = (root: TreeNode | null) => {
+      if (!root) return;
+
+      helper(root.left);
+      result.push(root.val);
+
+      helper(root.right);
+   };
+
+   helper(root);
+
+   return result;
+}
